b6cda02459b1ae0afd5584f874bc63cebb23881d
Bahram Jafari <bahramjafari2006@gmail.com>
2026-09-09 04:57:55 +0430

    Changes made in urls.py that images on database should work

--- my_site/urls.py
+++ my_site/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("blog.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + static (settings.STATIC_URL, document_root= settings.STATIC_ROOT)
+    path("", include("blog.urls")),
+
+    path(
+        "files/<path:path>",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
